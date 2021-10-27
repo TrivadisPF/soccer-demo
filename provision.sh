8dbf058a6b5641b5689b8cad47b324e280939816
gschmutz <guido.schmutz@trivadis.com>
2021-10-27 21:27:44 +0200

    Update provision.sh

--- provision.sh
+++ provision.sh
@@ -14,6 +14,11 @@ platys gen
 # copy streamsets plugins
 cp -r ../infrastructure/streamsets/user-libs/ plugins/streamsets/
 
+# copy docker-compose.override file
+cp  ../platys-platform/docker-compose.override.yml .
+
+cp -r ../platys-platform/restapi-mock/ ./scripts/
+
 # copy data files and unzip
 cp -r ../data/* ./data-transfer/
 unzip -P ${ZIP_PASSWORD} data-transfer/livestream-source-system/fixture-livestream.zip -d data-transfer
